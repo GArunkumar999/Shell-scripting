fd8790344ee6f154a776c3200feec115483c06c7
arun <arun@gmail.com>
2024-12-31 21:55:49 +0530

    addition of two numbers

--- 15-installing git.sh
+++ 15-installing git.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 #checking given user is root user or not
-
+id=$((dnf list installed))
 user=$(id -u)
 
 if [ $user -ne 0 ]
 then 
      echo "You have to be root user"
      exit 1
+    if [ $id -eq python ]
+      echo "python installed already"
 else 
     dnf install python -y
-     echo "Installing git successful"
+     echo "Installing python successful"
 fi
