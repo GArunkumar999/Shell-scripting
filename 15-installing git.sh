33f8b9766f07a71b394e842d9087de547328f9d2
arun <arun@gmail.com>
2024-12-31 21:48:05 +0530

    addition of two numbers

---
+++ 15-installing git.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#checking given user is root user or not
+
+user=$(id -u)
+
+if [ $user -ne 0 ]
+then 
+     echo "You have to be root user"
+else 
+     echo "Installing git successful"
+     
