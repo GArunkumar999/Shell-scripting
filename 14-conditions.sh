8b04c1df2fb85a0e774a1d490285d42a92043203
arun <arun@gmail.com>
2024-12-31 21:44:34 +0530

    addition of two numbers

---
+++ 14-conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#checking given number is positive or negative number or zero
+
+read -p "Enter a number: " NUMBER
+
+if [ $NUMBER -gt 0 ]
+then 
+     echo "Positive number"
+elif [ $NUMBER -lt 0]
+then 
+      echo "Negative number"
+else
+      echo "Zero number"
+fi
